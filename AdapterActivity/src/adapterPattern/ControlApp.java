f74a8de36c1ef5efe713555c0e90260ef5904af7
Janelle <janellesarcilla@gmail.com>
2026-09-24 18:28:24 +0800

    Commit

--- AdapterActivity/src/adapterPattern/ControlApp.java
+++ AdapterActivity/src/adapterPattern/ControlApp.java
@@ -4,13 +4,18 @@ public class ControlApp {
     public static void main(String[] args) {
         // Create instances of the devices
         SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+        PowerOutlet smartphoneOutlet = new SmartphoneAdapter(smartphoneCharger);
+
         Refrigerator refrigerator = new Refrigerator();
+        PowerOutlet refrigeratorOutlet = new RefrigeratorAdapter(refrigerator);
+
         Laptop laptop = new Laptop();
+        PowerOutlet laptopOutlet = new LaptopAdapter(laptop);
 
         // Use the devices
-        System.out.println(smartphoneCharger.chargePhone());
-        System.out.println(refrigerator.startCooling());
-        System.out.println(laptop.charge());
+        smartphoneOutlet.plugIn();
+        refrigeratorOutlet.plugIn();
+        laptopOutlet.plugIn();
     }
     
 }
---
+++ AdapterActivity/src/adapterPattern/LaptopAdapter.java
@@ -0,0 +1,15 @@
+package adapterPattern;
+
+public class LaptopAdapter implements PowerOutlet {
+    private Laptop laptop;
+
+    public LaptopAdapter(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    @Override
+    public void plugIn() {
+        System.out.println(laptop.charge());
+    }
+    
+}
